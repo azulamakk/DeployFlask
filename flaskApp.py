37a6397dc5684b2c2e1b5f8610a9e8480e339042
azulamakk <92961593+azulamakk@users.noreply.github.com>
2024-06-26 19:58:53 -0300

    fix: listen on 0.0.0.0

--- flaskApp.py
+++ flaskApp.py
@@ -104,4 +104,4 @@ def index():
                            unique_Upayment=unique_Upayment)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
